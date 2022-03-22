8a4d6d275cd853161fbc1c5dbb0843a87f593365
cahyafaisalreza <cf.reza07@gmail.com>
2022-03-22 15:53:30 +0700

    - Add ideas app - create a model mixin with url-related methods

---
+++ myproject/apps/ideas/models.py
@@ -0,0 +1,14 @@
+# myproject/apps/ideas/models.py
+from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from myproject.apps.core.models import UrlBase
+
+
+class Idea(UrlBase):
+    # fields, attributes, properties and methods...
+
+    def get_url_path(self):
+        return reverse("idea_details", kwargs={
+            "idea_id": str(self.pk),
+        })
