42c75bfe2ff8e25526c5c144bc8584a600ae6f6a
cahyafaisalreza <cf.reza07@gmail.com>
2022-03-22 20:14:23 +0700

    creating a model mixin to handle creation and modification dates

--- myproject/apps/core/models.py
+++ myproject/apps/core/models.py
@@ -2,6 +2,7 @@
 from urllib.parse import urlparse, urlunparse
 from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class UrlBase(models.Model):
    """
@@ -35,3 +36,21 @@ class UrlBase(models.Model):
 
    def get_absolute_url(self):
        return self.get_url()
+
+class CreationModificationDateBase(models.Model):
+    """
+    Abstract base class with a creation and modification date and time
+    """
+
+    created = models.DateTimeField(
+        _("Creation Date and Time"),
+        auto_now=True,
+    )
+
+    modified = models.DateTimeField(
+        _("Modification Date and Time"),
+        auto_now=True,
+    )
+
+    class Meta:
+        abstract = True
--- myproject/apps/ideas/models.py
+++ myproject/apps/ideas/models.py
@@ -2,10 +2,10 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from myproject.apps.core.models import UrlBase
+from myproject.apps.core.models import UrlBase, CreationModificationDateBase
 
 
-class Idea(UrlBase):
+class Idea(UrlBase, CreationModificationDateBase):
     # fields, attributes, properties and methods...
 
     def get_url_path(self):
