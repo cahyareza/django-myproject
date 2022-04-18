c4144732965b82789f5b36beae945686c59f4fe3
cahyafaisalreza <cf.reza07@gmail.com>
2022-04-18 09:54:18 +0700

    Chapter 3 Composing class-based views

--- myproject/apps/ideas/views.py
+++ myproject/apps/ideas/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.forms import modelformset_factory
-from django.views.generic import ListView, DetailView
+from django.views.generic import View, ListView, DetailView
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .forms import IdeaForm, IdeaTranslationsForm
 from .models import Idea, IdeaTranslations
@@ -72,58 +72,63 @@ def delete_idea(request, pk):
     context = {"idea": idea}
     return render(request, "ideas/idea_deleting_confirmation.html", context)
 
-def idea_list(request):
-    qs = Idea.objects.order_by("title")
-    form = IdeaFilterForm(data=request.GET)
+class IdeaListView(View):
+    form_class = IdeaFilterForm
+    template_name = "ideas/idea_list.html"
 
-    facets = {
-        "selected": {},
-        "categories": {
-            "authors": form.fields["author"].queryset,
-            "categories": form.fields["category"].queryset,
-            "ratings": RATING_CHOICES
-        },
-    }
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(data=request.GET)
+        qs, facets = self.get_queryset_and_facets(form)
+        page = self.get_page(request, qs)
+        context = {"form": form, "facets": facets,
+            "object_list": page}
+        return render(request, self.template_name, context)
 
-    if form.is_valid():
-        filters = (
-            # query parameter, filter parameter
-            ("author", "author"),
-            ("category", "categories"),
-            ("rating", "rating"),
-        )
-        qs = filter_facets(facets, qs, form, filters)
+    def get_queryset_and_facets(self, form):
+        qs = Idea.objects.order_by("title")
+        facets = {
+            "selected": {},
+            "categories": {
+                "authors": form.fields["author"].queryset,
+                "categories": form.fields["category"].queryset,
+                "ratings": RATING_CHOICES,
+            },
+        }
+        if form.is_valid():
+            filters = (
+                # query parameter, filter parameter
+                ("author", "author"),
+                ("category", "categories"),
+                ("rating", "rating"),
+            )
+            qs = self.filter_facets(facets, qs, form, filters)
+            return qs, facets
 
-    paginator = Paginator(qs, PAGE_SIZE)
-    page_number = request.GET.get("page")
-    try:
-        page = paginator.page(page_number)
-    except PageNotAnInteger:
-        # If page is not an integer, show first page.
-        page = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range, show last existing page.
-        page = paginator.page(paginator.num_pages)
+    @staticmethod
+    def filter_facets(facets, qs, form, filters):
+        for query_param, filter_param in filters:
+            value = form.cleaned_data[query_param]
+            if value:
+                selected_value = value
+                if query_param == "rating":
+                    rating = int(value)
+                    selected_value = (rating,
+                        dict(RATING_CHOICES)[rating])
+                facets["selected"][query_param] = selected_value
+                filter_args = {filter_param: value}
+                qs = qs.filter(**filter_args).distinct()
 
+        return qs
 
-    context = {
-        "form": form,
-        "facets": facets,
-        "object_list": page
-    }
-    return render(request, "ideas/idea_list.html", context)
-
-def filter_facets(facets, qs, form, filters):
-    for query_param, filter_param in filters:
-        value = form.cleaned_data[query_param]
-        if value:
-            selected_value = value
-            if query_param == "rating":
-                rating = int(value)
-                selected_value = (rating,
-                                  dict(RATING_CHOICES)[rating])
-            facets["selected"][query_param] = selected_value
-            filter_args = {filter_param: value}
-            qs = qs.filter(**filter_args).distinct()
-
-    return qs
+    def get_page(self, request, qs):
+        paginator = Paginator(qs, PAGE_SIZE)
+        page_number = request.GET.get("page")
+        try:
+            page = paginator.page(page_number)
+        except PageNotAnInteger:
+            # If page is not an integer, show first page.
+            page = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range, show last existing page.
+            page = paginator.page(paginator.num_pages)
+        return page
