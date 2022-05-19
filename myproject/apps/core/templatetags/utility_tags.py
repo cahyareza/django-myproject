7ff61f36511a1a949921cb2bdf50eb2ed16dc4c1
cahyafaisalreza <cf.reza07@gmail.com>
2022-05-20 04:42:22 +0700

    Chapter 5 Creating a template filter to show how many days have passed since a post was published

--- myproject/apps/core/templatetags/utility_tags.py
+++ myproject/apps/core/templatetags/utility_tags.py
@@ -1,8 +1,10 @@
 from urllib.parse import urlencode
-
+from datetime import datetime
 from django import template
 from django.utils.encoding import force_str
 from django.utils.safestring import mark_safe
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
 register = template.Library()
 
@@ -41,4 +43,43 @@ def modify_query(context, *params_to_remove, **params_to_change):
                     # attach new params
     for key, value in params_to_change.items():
         query_params.append((key, value))
-    return construct_query_string(context, query_params)
+    return construct_query_string(context, query_params)\
+
+"""FILTERS"""
+
+DAYS_PER_YEAR = 365
+DAYS_PER_MONTH = 30
+DAYS_PER_WEEK = 7
+
+@register.filter(is_safe=True)
+def date_since(specific_date):
+    """
+    Returns a human-friendly difference between today and past_date
+    (adapted from https://www.djangosnippets.org/snippets/116/)
+    """
+    today = timezone.now().date()
+    if isinstance(specific_date, datetime):
+        specific_date = specific_date.date()
+    diff = today - specific_date
+    diff_years = int(diff.days / DAYS_PER_YEAR)
+    diff_months = int(diff.days / DAYS_PER_MONTH)
+    diff_weeks = int(diff.days / DAYS_PER_WEEK)
+    diff_map = [
+        ("year", "years", diff_years,),
+        ("month", "months", diff_months,),
+        ("week", "weeks", diff_weeks,),
+        ("day", "days", diff.days,),
+    ]
+    for parts in diff_map:
+        (interval, intervals, count,) = parts
+        if count > 1:
+            return _(f"{count} {intervals} ago")
+        elif count == 1:
+            return _("yesterday") \
+                if interval == "day" \
+                else _(f"last {interval}")
+    if diff.days == 0:
+        return _("today")
+    else:
+        # Date is in the future; return formatted date.
+        return f"{specific_date:%B %d, %Y}"
